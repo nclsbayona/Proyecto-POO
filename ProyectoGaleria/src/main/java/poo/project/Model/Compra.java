bd8fb2eb3cbfa7b0b280d4b19f7030280c4a46f8
Nicolas Bayona <bayona.n@javeriana.edu.co>
2020-09-12 15:37:16 -0500

    Actualizando

--- ProyectoGaleria/src/main/java/poo/project/Model/Compra.java
+++ ProyectoGaleria/src/main/java/poo/project/Model/Compra.java
@@ -1,11 +1,46 @@
 package poo.project.Model;
 import java.util.Calendar;
+import java.util.HashSet;
 
 public class Compra {
-	private private long codigoCompra;
+
+	private HashSet<Cliente> clientes;
+	private HashSet<Obra> obras;
+	private long codigoCompra;
 	private Calendar fecha;
 	private boolean pagado;
+
+	//Accessor 
+
+	public HashSet<Cliente> getClientes()
+    {
+		return this.clientes;
+    }
+
+	public void setClientes()
+	{
+		this.clientes = clientes;
+    }
+
+    public HashSet<Obra> getObras()
+    {
+        return this.obras;
+    }
+    public void setObras(HashSet<Obra> obras)
+	{
+		this.obras  =  obras;
+	}
+
+	public void getCodigoCompra()
+	{
+		return this.codigoCompra;
+	}
+	public
+
+
 }
 
 
-// hola  andres porras
+
+
+
