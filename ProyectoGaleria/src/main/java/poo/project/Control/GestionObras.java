0844272d2b966acecab81380c4f7f2eb18d110c2
Nicolas Bayona <bayona.n@javeriana.edu.co>
2020-09-12 17:20:34 -0500

    Update GestionObras.java

--- ProyectoGaleria/src/main/java/poo/project/Control/GestionObras.java
+++ ProyectoGaleria/src/main/java/poo/project/Control/GestionObras.java
@@ -2,26 +2,27 @@ package poo.project.Control;
 
 import poo.project.Model.Obra;
 import poo.project.Model.Artista;
+import java.util.HashSet;
 import java.util.Calendar;
 
 public class GestionObras {
 
-    public Obra buscarObra(HashSet<Obra> listaObras, String titulo) {
+    //FALTA BUSCAR POR ESTADO 
+    public void buscarObra(HashSet<Obra> listaObras, String titulo) {
         for (Obra obra : listaObras) {
             if (obra.getTitulo().equals(titulo)) {
-                return obra;
+                System.out.println(obra);
             }
         }
-        return null;
     }
 
-    public Obra buscarObra(HashSet<Obra> listaObras, Calendar fecha) {
+    // BUSCAR SOLO POR AÑO- ME FALTA ESO-Y BUSCAR ESTADO
+    public void buscarObra(HashSet<Obra> listaObras, Calendar fecha) {
         for (Obra obra : listaObras) {
             if (obra.getFecha().equals(fecha)) {
                 System.out.println(obra);
             }
         }
-        return null;
     }
     /*
      * public Obra buscarObra(Artista artista) { for (Obra obra : listaObras) { if
@@ -36,12 +37,16 @@ public class GestionObras {
     // Ver listado de obras
     public void verListadoObras(HashSet<Obra> listaObras) {
         for (Obra obra : listaObras) {
-            System.out.println(obra);
+        //Solo deben salir las Obras que están disponibles para la Compra
+            if(/*Puede ser estado o puede ser una búsqueda*/)
+            {
+                System.out.println(obra);
+            }
         }
     }
-    //Crear Obra
-    public void crearObra(HashSet<Obra> listaObras,Obra obra)
-    {
+
+    // Crear Obra
+    public void crearObra(HashSet<Obra> listaObras, Obra obra) {
 
         listaObras.add(obra);
     }
