f3d4210bec9af6e89d0ba0b721c8fb82cce9c27e
Nicolas Bayona <bayona.n@javeriana.edu.co>
2020-09-12 15:13:52 -0500

    2

--- ProyectoGaleria/src/main/java/poo/project/Model/Compra.java
+++ ProyectoGaleria/src/main/java/poo/project/Model/Compra.java
@@ -3,6 +3,7 @@ import java.util.Calendar;
 
 public class Compra 
 {
+    private 
     private long codigoCompra;
     private Calendar fecha;
     private boolean pagado;
--- ProyectoGaleria/src/main/java/poo/project/Model/Obra.java
+++ ProyectoGaleria/src/main/java/poo/project/Model/Obra.java
@@ -1,11 +1,53 @@
 package poo.project.Model;
-import java.util.Calendar;
 
-public class Obra 
-{
+import java.util.Calendar;
+import java.util.HashSet;
+
+public class Obra {
+    private HashSet<Artista> artistas;
     private long codigoObra;
     private String titulo;
     private Calendar fecha;
     private float precioRef;
     private String dimensiones;
+
+    // Métodos accesors
+    public HashSet<Artista> getArtista() {
+        return this.artistas;
+    }
+
+    public void setArtista(HashSet<Artista> _artista) {
+        this.artistas = _artista;
+    }
+
+    public long getCodigoObra() {
+        return this.codigoObra;
+    }
+
+    public void setCodigoObra(long _codigoObra) {
+        this.codigoObra = _codigoObra;
+    }
+
+    public String getTitulo() {
+        return this.titulo;
+    }
+
+    public void setTitulo(String _titulo) {
+        this.titulo = _titulo;
+    }
+
+    public Calendar getCalendar() {
+        return this.fecha;
+    }
+
+    public void setCalendar(Calendar _fecha) {
+        this.fecha = _fecha;
+    }
+    public float getPrecioRef() {
+        return this.fecha;
+    }
+
+    public void setCalendar(Calendar _fecha) {
+        this.fecha = _fecha;
+    }
 }
