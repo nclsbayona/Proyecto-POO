be84ff5975f0057eadf603adf21b0140c8fecabd
Nicolas Bayona <bayona.n@javeriana.edu.co>
2020-10-23 23:24:10 -0500

    Proooooooo

--- Project/src/test/java/pruebas/ControlGaleriaTest.java
+++ Project/src/test/java/pruebas/ControlGaleriaTest.java
@@ -34,7 +34,7 @@ class ControlGaleriaTest {
 
 	/**
 	 * No hemos solucionado addCliente() crearCliente() modificarCliente()
-	 * ComprasAsociadosACuadro() 
+	 * ComprasAsociadosACuadro() modificarObra() 
 	 */
 
 	@Test
@@ -99,7 +99,7 @@ class ControlGaleriaTest {
 		fecha4.set(1999, 9, 22);
 		obras.add(new Cuadro(5432198, "Okalokas", fecha4.getTime(), 20000, "10x8", "Cubismo", "Pastel",
 				Clasificacion.OBRA_REPRESENTATIVA));
-		assertEquals(obras,this.controlGaleria.buscarObra(fecha));
+		assertEquals(obras, this.controlGaleria.buscarObra(fecha));
 	}
 
 	@Test
@@ -150,12 +150,23 @@ class ControlGaleriaTest {
 		assertNull(this.controlGaleria.addObra(expected));
 	}
 
+@Test
+	// Rectificar si existe compra, en este caso deberia ser verdadero
+	void existeCodCompra() {
+		Obra obra = this.controlGaleria.buscarObra(1234567);
+		Cliente c = new Cliente(100, Long.valueOf(143325536), "Jorge", "Muñoz", "tr 100 # 20-36", 98826628);
+		this.controlGaleria.realizarCompra(c, obra);
+		assertTrue(this.controlGaleria.existeCodCompra(1));
+	}
+
 	@Test
 	// Rectificar si existe compra, en este caso deberia ser falso
-	void existeCodCompra() {
+	void existeCodCompra2() {
 		assertFalse(this.controlGaleria.existeCodCompra(1));
 	}
 
+
+	
 	@Test
 	// Busca un cliente en las compras
 	void buscarClienteYObraEnCompra() {
@@ -164,6 +175,17 @@ class ControlGaleriaTest {
 				Clasificacion.OBRA_REPRESENTATIVA);
 		Cliente c = new Cliente(2, 1293723, "Fred", "Jones", "20822 SW Luxury Park", 98765432);
 		assertFalse(this.controlGaleria.buscarClienteYObraEnCompra(c, o));
+	}
+
+	@Test
+	// Busca un cliente en las compras
+	void buscarClienteYObraEnCompra2() {
+		Calendar fecha3 = Calendar.getInstance();
+		Obra o = new Cuadro(1234567, "Michuelo", fecha3.getTime(), 20000, "20x5", "Cubismo", "Pastel",
+				Clasificacion.OBRA_REPRESENTATIVA);
+		Cliente c = new Cliente(2, 1293723, "Fred", "Jones", "20822 SW Luxury Park", 98765432);
+		this.controlGaleria.realizarCompra(c, o);
+		assertTrue(this.controlGaleria.buscarClienteYObraEnCompra(c, o));
 	}
 
 	@Test
@@ -181,21 +203,21 @@ class ControlGaleriaTest {
 
 	@Test
 	void testBuscarObraPorArtista2() {
-		HashSet<Obra> expected=new HashSet<Obra>();
-		assertEquals(expected,this.controlGaleria.buscarObraporArtista("Natalia"));
+		HashSet<Obra> expected = new HashSet<Obra>();
+		assertEquals(expected, this.controlGaleria.buscarObraporArtista("Natalia"));
 	}
 
 	// Deberia funcionar
 	@Test
 	void testBuscarEscultura() {
-		HashSet<Escultura> expected=new HashSet<Escultura>();
+		HashSet<Escultura> expected = new HashSet<Escultura>();
 		Calendar fecha2 = Calendar.getInstance();
 		Calendar fecha5 = Calendar.getInstance();
-	    fecha2.set(2001, 8, 6);
-	    fecha5.set(1984, 2, 3);
+		fecha2.set(2001, 8, 6);
+		fecha5.set(1984, 2, 3);
 		expected.add(new Escultura(3456789, "Sociopata", fecha2.getTime(), 15000, "10x2", "Cemento", 2084));
 		expected.add(new Escultura(7654321, "Machupichu", fecha5.getTime(), 15000, "10x2", "Marmol", 1550));
-		assertEquals(expected,this.controlGaleria.buscarEsculturas());
+		assertEquals(expected, this.controlGaleria.buscarEsculturas());
 	}
 
 	// No deberia funcionar
@@ -204,17 +226,94 @@ class ControlGaleriaTest {
 		HashSet<Obra> expected = new HashSet<>();
 		assertNotEquals(expected, this.controlGaleria.buscarEsculturas());
 	}
-	//Funciona
+
+	// Funciona
 	@Test
-	void testPrecioTotal(){
-		double expected=653700;
-		assertEquals(expected,this.controlGaleria.calcularPrecioTotal());
-	
+	void testPrecioTotal() {
+		double expected = 653700;
+		assertEquals(expected, this.controlGaleria.calcularPrecioTotal());
+
+	}
+
+	// No funciona
+	@Test
+	void testPrecioTotal2() {
+		double expected = -1;
+		assertNotEquals(expected, this.controlGaleria.calcularPrecioTotal());
+	}
+
+	// Funciona
+	@Test
+	void testBuscarArtista() {
+		Calendar fecha = Calendar.getInstance();
+		Artista expected = new Artista(Long.parseLong("1000471976"), "Sebastian", "Herrera Guaitero", fecha, 350612646);
+		assertEquals(expected, this.controlGaleria.buscarArtista(Long.valueOf("1000471976")));
+	}
+
+	// No funciona
+	@Test
+	void testBuscarArtista2() {
+		assertNull(this.controlGaleria.buscarArtista(1000000000));
+	}
+
+	// Funciona
+	@Test
+	void testAddObra(){
+		Calendar fecha=Calendar.getInstance();
+		 fecha.set(2020, 11, 01);
+        Obra o=new Instalacion(1111111, "Probando", fecha.getTime(), 20000, "20x5", "Prueba de instalacion");
+	  assertEquals(o,this.controlGaleria.addObra(new Instalacion(1111111, "Probando", fecha.getTime(), 20000, "20x5", "Prueba de instalacion")));
+		
 	}
 	//No funciona
 	@Test
-	void testPrecioTotal2(){
-		double expected=-1;
-		assertNotEquals(expected, this.controlGaleria.calcularPrecioTotal());
+	void testAddObra2(){
+		Calendar fecha2 =  Calendar.getInstance();
+		assertNull(this.controlGaleria.addObra(new Escultura(3456789, "Sociopata", fecha2.getTime(), 15000, "10x2", "Cemento", 2084)));
 	}
+	@Test
+	// Eliminar obra, debe eliminarlo
+	void testEliminarObra() {
+		assertTrue(this.controlGaleria.eliminarObra(1234567));
+	}
+
+	@Test
+	// Eliminar obra que no existe, no deberia eliminar
+	void testEliminarObra2() {
+		assertFalse(this.controlGaleria.eliminarObra(Long.valueOf("9991872")));
+	}
+	@Test
+	// Buscar compra, debe encontrarlo
+	void testBuscarCompra() {
+		Calendar fecha3 = Calendar.getInstance();
+		Obra o = new Cuadro(1234567, "Michuelo", fecha3.getTime(), 20000, "20x5", "Cubismo", "Pastel",
+				Clasificacion.OBRA_REPRESENTATIVA);
+		Cliente c = new Cliente(2, 1293723, "Fred", "Jones", "20822 SW Luxury Park", 98765432);
+		this.controlGaleria.realizarCompra(c, o);
+		assertEquals(this.controlGaleria.buscarCompra("1"), this.controlGaleria.buscarCompra("1"));;
+	}
+
+	@Test
+	// Buscar compra que no existe, no deberia encontrar
+	void testBuscarCompra2() {
+		assertNull(this.controlGaleria.buscarCompra("0"));
+	}
+	
+	@Test
+	// Eliminar compra, debe encontrarlo
+	void testEliminarCompra() {
+		Calendar fecha3 = Calendar.getInstance();
+		Obra o = new Cuadro(1234567, "Michuelo", fecha3.getTime(), 20000, "20x5", "Cubismo", "Pastel",
+				Clasificacion.OBRA_REPRESENTATIVA);
+		Cliente c = new Cliente(2, 1293723, "Fred", "Jones", "20822 SW Luxury Park", 98765432);
+		this.controlGaleria.realizarCompra(c, o);
+		assertNotNull(this.controlGaleria.eliminCompra("1"));
+	}
+
+	@Test
+	// Eliminar compra que no existe, no deberia encontrar
+	void testEliminarCompra2() {
+		assertNull(this.controlGaleria.eliminCompra("0"));
+	}
+				
 }
