cb7788871793c18e96a4517fbe10b9ea97fb078a
Nicolas Bayona <bayona.n@javeriana.edu.co>
2020-10-23 20:19:01 -0500

    Cambio paquetes

---
+++ Project/src/test/java/pruebas/ControlGaleriaTest.java
@@ -0,0 +1,267 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class ControlGaleriaTest {
+
+	@BeforeAll
+	static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterAll
+	static void tearDownAfterClass() throws Exception {
+	}
+
+	@BeforeEach
+	void setUp() throws Exception {
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	@Test
+	void testStartDay() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarClienteLongString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testEliminarCliente() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddCliente() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCrearCliente() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testModificarCliente() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarClienteLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testOrganizarListaClientes() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarObraString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarObraLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarObraCalendar() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testObraEnCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarObraporArtista() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddCircObryArt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertarObraStringStringStringStringStringStringStringStringArtistaStringStringClasificacion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertarObraStringStringStringStringStringStringStringStringStringStringStringStringStringClasificacion() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertarObraStringStringStringStringStringStringStringStringArtistaString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertarObraStringStringStringStringStringStringStringStringStringStringStringString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertarObraStringStringStringStringStringStringStringStringArtistaStringDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testInsertarObraStringStringStringStringStringStringStringStringStringStringStringStringDouble() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarEsculturas() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testComprasAsociadasACuadro() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testCalcularPrecioTotal() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarArtista() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testAddObra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testModificarObra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testExisteCodCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarClienteYObraEnCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testEliminarObra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testEliminCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testBuscarObraEnCompras() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testRealizarCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testListadoDeCompra() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testVerListadoArtistas() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testControlGaleria() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testObject() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testGetClass() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testHashCode() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testEquals() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testClone() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testNotify() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testNotifyAll() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testWait() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testWaitLong() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testWaitLongInt() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	void testFinalize() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
