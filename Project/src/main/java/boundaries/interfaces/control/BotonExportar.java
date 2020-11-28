2b53cf8000017f73de46572bd16b9674df3722ea
AtonCode <49330231+AtonCode@users.noreply.github.com>
2020-11-27 20:37:33 -0500

    Boton Export

--- Project/src/main/java/boundaries/interfaces/control/BotonExportar.java
+++ Project/src/main/java/boundaries/interfaces/control/BotonExportar.java
@@ -21,6 +21,12 @@ public class BotonExportar {
 
     @FXML
     private Button btn_Exportar;
+    @FXML
+    private Button btn_ExportarF;
+    @FXML
+    private Button btn_ExportarM;
+    @FXML
+    private Button btn_ExportarO;
 
     @FXML
     void exportarA(ActionEvent event) {
@@ -36,10 +42,57 @@ public class BotonExportar {
             e.printStackTrace();
         }
     }
+    @FXML
+    void exportarF(ActionEvent event) {
+        String ruta = "";
+        ControlGaleria cGaleria = new ControlGaleria();
+        Exportacion cExportacion = new Exportacion();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Report File");
+        ruta = String.valueOf(fileChooser.showSaveDialog(null));
+        try {
+            cExportacion.exportarXML(ruta, "f", cGaleria.getListaClientes().values());
+        } catch (TypoException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    void exportarM(ActionEvent event) {
+        String ruta = "";
+        ControlGaleria cGaleria = new ControlGaleria();
+        Exportacion cExportacion = new Exportacion();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Report File");
+        ruta = String.valueOf(fileChooser.showSaveDialog(null));
+        try {
+            cExportacion.exportarXML(ruta, "m", cGaleria.getListaCompras());
+        } catch (TypoException e) {
+            e.printStackTrace();
+        }
+    }
+    @FXML
+    void exportarO(ActionEvent event) {
+        String ruta = "";
+        ControlGaleria cGaleria = new ControlGaleria();
+        Exportacion cExportacion = new Exportacion();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select Report File");
+        ruta = String.valueOf(fileChooser.showSaveDialog(null));
+        try {
+            cExportacion.exportarXML(ruta, "o", cGaleria.getListaArtistas().values());
+        } catch (TypoException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     void initialize() {
         assert btn_Exportar != null : "fx:id=\"btn_Exportar\" was not injected: check your FXML file 'Hero.fxml'.";
+        assert btn_ExportarF != null : "fx:id=\"btn_ExportarF\" was not injected: check your FXML file 'Hero.fxml'.";
+        assert btn_ExportarM != null : "fx:id=\"btn_ExportarM\" was not injected: check your FXML file 'Hero.fxml'.";
+        assert btn_ExportarO != null : "fx:id=\"btn_ExportarO\" was not injected: check your FXML file 'Hero.fxml'.";
 
     }
+
+
 }
